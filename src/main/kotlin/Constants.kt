24ed52f0e15c7bb298d1bfeed5a8de95b51e0ea9
Flashspeed <Flashspeed@users.noreply.github.com>
2022-09-23 14:14:40 +0100

    Video download working. Tidy. Pulled strings into constants.

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,13 @@
+object Constants {
+
+    object Buttons{
+        const val START = "START"
+        const val STOP = "STOP"
+    }
+
+    const val SAVE_LOCATION = "Save Location"
+    const val INVALID_FILE_PATH = "Invalid path"
+    const val URL_PLACEHOLDER = "Paste URL here"
+    const val AUDIO_FORMAT = "Audio format"
+    const val VIDEO_FORMAT = "Video format"
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import Constants.AUDIO_FORMAT
+import Constants.Buttons.START
+import Constants.Buttons.STOP
+import Constants.INVALID_FILE_PATH
+import Constants.SAVE_LOCATION
+import Constants.URL_PLACEHOLDER
+import Constants.VIDEO_FORMAT
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -12,9 +18,17 @@ import components.DropDown
 import components.FilePicker
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import java.io.BufferedReader
 import java.io.File
-import java.util.concurrent.TimeUnit
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 @Composable
 @Preview
@@ -22,6 +36,9 @@ fun App() {
     var url by remember { mutableStateOf("") }
     var isFileChooserOpen by remember { mutableStateOf(false) }
     var fileChooserButtonClicked by remember { mutableStateOf(false) }
+    var downloadPercentage by remember { mutableStateOf("0") }
+    var downloadButtonText by remember { mutableStateOf("START") }
+    var isDownloading by remember { mutableStateOf(false) }
     var filePath: String? by remember { mutableStateOf("") }
     val myCoroutineScope = rememberCoroutineScope()
 
@@ -30,17 +47,18 @@ fun App() {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 32.dp, vertical = 24.dp)
-        ){
+        ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-            ){
+            ) {
+                // URL bar
                 TextField(
                     modifier = Modifier.weight(1f),
                     value = url,
-                    onValueChange = { url = it},
+                    onValueChange = { url = it },
                     placeholder = {
-                        Text("Paste URL here")
+                        Text(URL_PLACEHOLDER)
                     }
                 )
             }
@@ -50,16 +68,16 @@ fun App() {
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier.fillMaxWidth()
-            ){
+            ) {
                 DropDown(
-                    dropDownTitle = "Audio format",
+                    dropDownTitle = AUDIO_FORMAT,
                     dropDownOptions = listOf("MP3"),
                     onOptionSelected = {
                         println(it)
                     }
                 )
                 DropDown(
-                    dropDownTitle = "Video format",
+                    dropDownTitle = VIDEO_FORMAT,
                     dropDownOptions = listOf("MP4"),
                     onOptionSelected = {
                         println(it)
@@ -71,8 +89,8 @@ fun App() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
-            ){
-                if(fileChooserButtonClicked){
+            ) {
+                if (fileChooserButtonClicked) {
                     FilePicker(
                         onCloseRequest = { _filePath ->
                             isFileChooserOpen = false
@@ -95,10 +113,10 @@ fun App() {
                         //Open file picker
                         fileChooserButtonClicked = true
                     }
-                ){
-                    Text("Save Location")
+                ) {
+                    Text(SAVE_LOCATION)
                 }
-                Text(text = filePath ?: "Invalid path")
+                Text(text = filePath ?: INVALID_FILE_PATH)
 
                 Divider(Modifier.padding(vertical = 30.dp))
 
@@ -106,20 +124,36 @@ fun App() {
                 Button(
                     modifier = Modifier.padding(horizontal = 8.dp),
                     onClick = {
-                        myCoroutineScope.launch(Dispatchers.IO) {
-                            fef()
+                        downloadButtonText = if (downloadButtonText == START) STOP else START
+                        if (!isDownloading) {
+                            isDownloading = true
+
+                            // TODO logic to check if the filename that the download would produce
+                            //  already exists and if it does then not to start download
+                            myCoroutineScope.launch(Dispatchers.IO) {
+                                launch {
+                                    startDownload().collect { _downloadPercentage ->
+                                        if (_downloadPercentage == "100") {
+                                            isDownloading = false
+                                            downloadButtonText = START
+                                        }
+                                        downloadPercentage = _downloadPercentage
+                                        println("Download Percentage: ${downloadPercentage}%")
+                                    }
+                                }
+                            }
                         }
                     }
-                ){
-                    Text("START")
+                ) {
+                    Text(text = downloadButtonText)
                 }
 
-                DownloadProgress(
+                DownloadProgressIndicator(
+                    percentValue = downloadPercentage,
                     onDownloadError = { errorMessage ->
                         println(errorMessage)
                     },
                     onDownloadComplete = {
-                        println("Download complete")
                     }
                 )
 
@@ -128,7 +162,7 @@ fun App() {
     }
 }
 
-suspend fun fef(){
+suspend fun startDownload(): Flow<String> = channelFlow {
     val downloadFolder = "/Users/ife/Documents/mediaDownloader"
     coroutineScope {
         launch(Dispatchers.IO) {
@@ -139,29 +173,96 @@ suspend fun fef(){
 //            val pwdCommandProcess = pwdCommand.start()
 
 
-            val youtubeDlCommand = ProcessBuilder("youtube-dl", "--format", "mp4", "https://www.youtube.com/watch?v=bhrumYeZvjs")
+            val youtubeDlCommand =
+                ProcessBuilder("youtube-dl", "--format", "mp4", "https://www.youtube.com/watch?v=bhrumYeZvjs")
             youtubeDlCommand.directory(File(downloadFolder))
-            youtubeDlCommand.redirectOutput(File("${downloadFolder}/downloader.log"))
             val youtubeDlCommandProcess = youtubeDlCommand.start()
 
-
-//            val fe = ProcessBuilder("youtube-dl", "--format", "mp4", "https://www.youtube.com/watch?v=bhrumYeZvjs").start()
-//            val result = println(fe.inputStream)
-
+            getOutputFromCommand(youtubeDlCommandProcess).collect { outputFromCommand ->
+                if (outputFromCommand.contains("[download]")) {
+                    send(findDownloadProgress(outputFromCommand))
+                }
+                if (outputFromCommand.contains("[download] 100%")) {
+                    println("DOWNLOAD COMPLETE")
+                }
+            }
         }
 
     }
 }
 
+fun getOutputFromCommand(process: Process): Flow<String> = flow {
+    println("process output: ${process.outputStream}")
+
+    val stdInput = BufferedReader(InputStreamReader(process.inputStream))
+    var outputFromCommand: String
+
+    try {
+        while (true) {
+            outputFromCommand = stdInput.readLine() ?: break
+            emit(outputFromCommand)
+        }
+        process.destroy()
+    } catch (e: IOException) {
+        println("ERROR OCCURRED: ${e.printStackTrace()}")
+    }
+}
+    .flowOn(Dispatchers.IO)
+
+/**
+ * Returns string value representing the download progress
+ *
+ * @param String String to search for download percentage e.g "[download] 34.9% ..."
+ * @return String - containing download percentage value e.g "39" without the percentage sign
+ */
+private fun findDownloadProgress(str: String): String {
+
+    var percentValue = "0"
+
+    // Pattern to find the line that contains the string [download]
+    val regexPattern1 = "^(\\[download])\\s+(\\d+(\\.)?\\d%)"
+    val pattern1: Pattern = Pattern.compile(regexPattern1)
+    val matcher1: Matcher = pattern1.matcher(str)
+
+    val extractedStringFromPass1: String =
+        if (matcher1.find()) {
+            matcher1.group(0)
+        } else {
+            // No match
+            percentValue
+        }
+
+    println("extractedStringFromPass1: $extractedStringFromPass1")
+
+    val regexPattern2 = "\\b(?<!\\.)(?!0+(?:\\.0+)?%)(?:\\d|[1-9]\\d|100)(?:(?<!100)\\.\\d+)?%"
+    val pattern2: Pattern = Pattern.compile(regexPattern2)
+    val matcher2: Matcher = pattern2.matcher(extractedStringFromPass1)
+
+    if (matcher2.find()) {
+        percentValue = matcher2.group(0)
+    } else {
+        // No match
+    }
+
+    return percentValue.replace("%", "")
+}
+
 @Composable
 @Preview
-fun DownloadProgress(
+fun DownloadProgressIndicator(
     modifier: Modifier = Modifier,
-    percentValue: Int = 0,
-    onDownloadComplete: ()->Unit,
-    onDownloadError: (errorMessage: String)->Unit,
-){
-    Text("%s%%".format(percentValue))
+    percentValue: String = "0",
+    onDownloadComplete: () -> Unit,
+    onDownloadError: (errorMessage: String) -> Unit,
+) {
+    Text(
+        if (percentValue == "100") {
+            "DOWNLOAD COMPLETE"
+        } else {
+            "${percentValue}%"
+        }
+    )
+
 }
 
 fun main() = application {
